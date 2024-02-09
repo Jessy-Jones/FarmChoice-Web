a08745239a07145ff9fd6e5a1fb6ee3ee55929dc
Jessy-Jones <93819297+Jessy-Jones@users.noreply.github.com>
2024-02-09 22:54:36 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const splashScreen = document.querySelector(".splash");
+splashScreen.addEventListener ('click', () => {
+    splashScreen.style.opacity = 0;
+    setTimeout(() => {
+        splashScreen.classList.add('hidden')
+    }, 610)
+})
